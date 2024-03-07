4073c255605a598be76a7737ca96157a1ea160bc
Satyam Thakur <sat.thakur20@gmail.com>
2024-03-07 16:25:32 +0530

    First commit

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectedCurrency = "usd",
+    amountDisabled = false,
+    currencyDisabled = false,
+    className = "",
+}) {
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className='w-1-2'>
+            <label className='text-black/40 mb-2 inline-block'>{label}</label>
+        </div>
+    </div>
+  )
+}
+
+export default InputBox
