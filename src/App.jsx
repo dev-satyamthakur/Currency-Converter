c772767cc854074e41d75709a7df0b6bb9ad3801
Satyam Thakur <sat.thakur20@gmail.com>
2024-03-09 09:15:11 +0530

    Adding input box form on app screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import useCurrencyInfo from './hooks/useCurrencyInfo';
+import { InputBox } from './components/index.js';
 
 function App() {
 
@@ -27,7 +28,38 @@ function App() {
     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' 
     style={{backgroundImage: `url(https://images.pexels.com/photos/259100/pexels-photo-259100.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`}}>
 
-
+    <div className="w-full">
+      <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          convert();
+        }}>
+          <div className='w-full mb-1'>
+            <InputBox 
+              label="from"
+              amount={amount}
+              currencyOptions={options}
+              onCurrencyChange={(currency) => setFromCurrency(currency)}
+              onAmountChange={(amount) => setAmount(amount)}
+              selectedCurrency={fromCurrency}
+            />
+          </div>
+          <div className='relative w-full h-0.5'>
+            <button className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5' onClick={swap}>Swap</button>
+          </div>
+          <div className='w-full mb-1'>
+            <InputBox 
+              label="to"
+              amount={amount}
+              currencyOptions={options}
+              onCurrencyChange={(currency) => setToCurrency(currency)}
+              onAmountChange={(amount) => setAmount(amount)}
+              selectedCurrency={toCurrency}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
 
     </div>
   )
